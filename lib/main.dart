f2958fc69ed5529e23ec0db7c958e14f1e5b97ab
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-20 12:40:15 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,195 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'models/global.dart';
+import 'models/technician.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  // This widget is the root of your application.
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+                myLocationButtonEnabled: false,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 400),
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 20.0,
+                        ),
+                      ],
+                      color: Colors.white,
+                    ),
+                    height: 50,
+                    width: 300,
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      decoration: InputDecoration(
+                          hintText: "What are you looking for?",
+                          hintStyle: TextStyle(fontFamily: 'Gotham', fontSize: 15),
+                          icon: Icon(Icons.search, color: Colors.black,),
+                          border: InputBorder.none,
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent))
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 550, bottom: 50),
+                child: ListView(
+                  padding: EdgeInsets.only(left: 20),
+                  children: getTechniciansInArea(),
+                  scrollDirection: Axis.horizontal,
+                ),
+              )
+            ],
+          )
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Technician> getTechies() {
+    List<Technician> techies = [];
+    for (int i = 0; i < 10; i++) {
+      AssetImage profilePic = new AssetImage("lib/assets/profile.png");
+      Technician myTechy = new Technician("Carlos Teller", "070-379-031", "First road 23 elm street", 529.3, 4, "Available", profilePic, "Electrician");
+      techies.add(myTechy);
+    }
+    return techies;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  List<Widget> getTechniciansInArea() {
+    List<Technician> techies = getTechies();
+    List<Widget> cards = [];
+    for (Technician techy in techies) {
+      cards.add(technicianCard(techy));
+    }
+    return cards;
+  }
+}
+
+Map statusStyles = {
+  'Available' : statusAvailableStyle,
+  'Unavailable' : statusUnavailableStyle
+};
+
+
+Widget technicianCard(Technician technician) {
+  return Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.only(right: 20),
+    width: 180,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      color: Colors.white,
+      boxShadow: [
+        new BoxShadow(
+          color: Colors.grey,
+          blurRadius: 20.0,
+        ),
+      ],
+    ),
+    child:
+    Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: CircleAvatar(
+                backgroundImage: technician.profilePic,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(technician.name, style: techCardTitleStyle,),
+                Text(technician.occupation, style: techCardSubTitleStyle,)
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Container(
+          margin: EdgeInsets.only(top: 30),
+          child: Row(
+            children: <Widget>[
+              Text("Status:  ", style: techCardSubTitleStyle,),
+              Text(technician.status, style: statusStyles[technician.status])
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text("Rating: " + technician.rating.toString(), style: techCardSubTitleStyle,)
+                ],
+              ),
+              Row(
+                  children: getRatings(technician)
+              )
+
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+List<Widget> getRatings(Technician techy) {
+  List<Widget> ratings = [];
+  for (int i = 0; i < 5; i++) {
+    if (i < techy.rating) {
+      ratings.add(
+          new Icon(
+              Icons.star,
+              color: Colors.yellow
+          )
+      );
+    } else {
+      ratings.add(
+          new Icon(
+              Icons.star_border,
+              color: Colors.black
+          )
+      );
+    }
   }
+  return ratings;
 }
